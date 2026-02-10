fd5bc7ef73aeb967aa71af4be8688d54cc88aec8
Jaibhullar <jai.bhullar@brighthr.com>
2026-02-10 12:37:22 +0000

    choose theme and create header

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+
+export type ContainerProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export const Container = ({ children, className }: ContainerProps) => {
+  return (
+    <div className={cn("w-[90vw] max-w-295 mx-auto", className)}>
+      {children}
+    </div>
+  );
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
+import { CalendarIcon } from "lucide-react";
+import { Container } from "../Container";
+
 export const Header = () => {
-  return <div className="bg-background"></div>;
+  return (
+    <div className="py-4">
+      <Container className="flex items-center gap-3">
+        <div className="p-2 rounded-md bg-primary w-fit">
+          <CalendarIcon className="w-7 h-7 text-white" />
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold">Absence Management</h1>
+          <p className="text-muted-foreground text-sm">
+            Track and manage employee absences
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
 };
